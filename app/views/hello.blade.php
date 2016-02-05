f932f3f50babdb89527f695ca05c5425e2f8eb01
pascalallen <thomaspascalallen@yahoo.com>
2016-02-05 17:55:11 -0600

    first commit

---
+++ app/views/hello.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Tattoos By Drew</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- BOOTSTRAP --}}
+	<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+	{{-- CUSTOM CSS --}}
+	<link href="/css/hello.css" type="text/css" rel="stylesheet">
+
+	{{-- CUSTOM FONT --}}
+	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+	{{-- FONT AWESOME --}}
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+</head>
+<body>
+	<h1>Tattoos By Drew</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
