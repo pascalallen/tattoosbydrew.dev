2bc6a9e4dd887536aad0dfaeb7d0e0417da3c8c7
pascalallen <thomaspascalallen@yahoo.com>
2016-06-14 15:24:13 -0500

    Provided docs for portfolio page so that i know what im doing later on

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -1,11 +1,22 @@
 @extends('layouts.master')
 
 <?php
+	//store directory to variable
 	$dir = "img/*.JPG";
+
+	//returns array matching $dir
 	$images = glob($dir);
+
+	//store how many images to show per page to variable
 	$perPage = 8;
+
+	//sets current page
 	$currentPage = Input::has('page') ? Input::get('page') - 1 : 1;
+
+	//returns the sequence of elements from the array as specified by the offset and length parameters
 	$pagedData = array_slice($images, $currentPage * $perPage, $perPage);
+
+	//create a pagination instance, pass an array of items($pagedData), total number of items(count($images)), and how many per page to display($perPage)
 	$images = Paginator::make($pagedData, count($images), $perPage);
 ?>
 
